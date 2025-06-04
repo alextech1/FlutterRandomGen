a9e586d90ca2b198853b89ccfd730aac8fb8187e
alextech1 <alejandro.hernandez7432@gmail.com>
2025-06-04 10:14:31 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.cyan),
+      home: const RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({Key? key}) : super(key: key);
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Random Generator')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('FAB');
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+//15:50
